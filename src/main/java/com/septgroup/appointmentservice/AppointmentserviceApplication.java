fcf82a4b052943cd150df414b8708998fadaaa1a
huntermuze <mbyamen@gmail.com>
2022-09-08 11:38:58 +1000

    setup CI

--- src/main/java/com/septgroup/appointmentservice/AppointmentserviceApplication.java
+++ src/main/java/com/septgroup/appointmentservice/AppointmentserviceApplication.java
@@ -2,12 +2,15 @@ package com.septgroup.appointmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// CTRL + SHIFT + G NEW BIND FOR REFORMAT CODE.
 @SpringBootApplication
+@EnableEurekaClient
 public class AppointmentserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointmentserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentserviceApplication.class, args);
+    }
 
 }
